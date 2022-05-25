0dbca42f2ae1dc2a6a75b6dd76565669cb390edf
Calebe Lino <calebe.n.lino@gmail.com>
2022-05-25 13:36:40 -0300

    Mirroring in the middle

--- imageFunctions.py
+++ imageFunctions.py
@@ -1,12 +1,19 @@
 import cv2
+import numpy as np
 
 
 def mirror_img(img):
-    img = img[:, ::-1, :]
+    for channel in range(np.shape(img)[2]):
+        print("Channel: ", channel)
+        for x in range(np.shape(img)[1]):
+            print("X: ", x)
+            img[:, x, channel] = img[:, np.shape(img)[1]-1-x, channel]
     return img
 
 
 test_img = cv2.imread("testImg.png")
-cv2.imwrite("mirrored.png", mirror_img(test_img))
+cv2.imshow("Mirrored", mirror_img(test_img))
+cv2.imwrite("centerMirrored.png", mirror_img(test_img))
+cv2.waitKey(0)
 
 
