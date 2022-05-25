0657aaa67f415458616515c9caec0195ce87de74
Calebe Lino <calebe.n.lino@gmail.com>
2022-05-25 12:46:50 -0300

    Displaying red channel only

--- imageFunctions.py
+++ imageFunctions.py
@@ -2,7 +2,8 @@ import cv2
 import numpy as np
 
 test_img = cv2.imread("testImg.png")
-cv2.imshow("Load", test_img)
+cv2.imshow("R", test_img[:, :, 2])
+print("Displaying red channel")
 cv2.waitKey(0)
 
-print("Image matrix dimensions:", np.ndim(test_img))
+
