b31ffad7d6e18da0a199e55bd89c905088c2f8e3
Calebe Lino <calebe.n.lino@gmail.com>
2022-05-26 20:44:05 -0300

    Implemented shadowing, modding and sqrt

--- imageFunctions.py
+++ imageFunctions.py
@@ -9,8 +9,6 @@ def clone_img(img):
         for y in range(np.shape(img)[0]):   # Each 'y' represents a line (group of values in a depth)
             for x in range(np.shape(img)[1]):   # Each 'x' represents a column (values in a line)
                 clone[y, x, z] = img[y, x, z]   # Copying each pixel to the 'clone' matrix. Pixels are accessed as [line, column, depth]
-    cv2.imshow("", clone)
-    cv2.waitKey(0)
     return clone
 
 
@@ -30,7 +28,7 @@ def reverse_img(img):
 
 
 def midmirror_img(img):
-    midmirr = img
+    midmirr = clone_img(img)
 
     for z in range(np.shape(img)[2]):
         for y in range(np.shape(img)[0]):
@@ -61,7 +59,7 @@ def rotate90(img):
 
 def avg_blur(img):
     # This function uses a 3x3 kernel for 'blur average' effect
-    blur = img
+    blur = clone_img(img)
 
     sumt = 0
     for z in range(np.shape(img)[2]):
@@ -70,7 +68,6 @@ def avg_blur(img):
             x = 0   # Reset x everytime next line is called
             while x < np.shape(img)[1] - (np.shape(img)[1] % 3):    # ''
                 sumt += img[y, x, z]    # Summation of pixels in a kernel
-                #print("img[",y, x, z,"]")
                 if (x+1) % 3 == 0:  # When reached the end of kernel column (it means that 'x' value is divisible by 3)
                     if (y+1) % 3 == 0:  # When reached the end of kernel lines ('' 'y' '')
                         avg = int(sumt / 9)     # Since there is 3x3 = 9 elements, divide the summation by 9
@@ -97,12 +94,83 @@ def avg_blur(img):
     return blur
 
 
+def shadowing(img, value):
+    shadow = clone_img(img)
+
+    for z in range(np.shape(img)[2]):
+        for y in range(np.shape(img)[0]):
+            for x in range(np.shape(img)[1]):
+                if img[y, x, z] >= value:
+                    shadow[y, x, z] = 0
+
+    cv2.imshow("", shadow)
+    print(img)
+    print("-------")
+    print(shadow)
+    cv2.waitKey(0)
+    return shadow
+
+
+def shadowing_v2(img, value):
+    shadow2 = clone_img(img)
+
+    for z in range(np.shape(img)[2]):
+        for y in range(np.shape(img)[0]):
+            for x in range(np.shape(img)[1]):
+                if img[y, x, z] >= value:
+                    shadow2[y, x, z] = img[y, x, z] - value
+
+    cv2.imshow("", shadow2)
+    print(img)
+    print("-------")
+    print(shadow2)
+    cv2.waitKey(0)
+    return shadow2
+
+
+def mod_img(img, number):
+    modded = clone_img(img)
+
+    for z in range(np.shape(img)[2]):
+        for y in range(np.shape(img)[0]):
+            for x in range(np.shape(img)[1]):
+                if img[y, x, z] % number == 0:
+                    modded[y, x, z] = 0
+
+    cv2.imshow("", modded)
+    print(img)
+    print("-------")
+    print(modded)
+    cv2.waitKey(0)
+    return modded
+
+
+def sqrt_img(img):
+    rooted2 = clone_img(img)
+
+    for z in range(np.shape(img)[2]):
+        for y in range(np.shape(img)[0]):
+            for x in range(np.shape(img)[1]):
+                rooted2[y, x, z] = int(np.sqrt(img[y, x, z])) * 10
+
+    cv2.imshow("", rooted2)
+    print(img)
+    print("-------")
+    print(rooted2)
+    cv2.waitKey(0)
+    return rooted2
+
+
 test_img = cv2.imread("testImg.png")
+cv2.imshow("Original", test_img)
+cv2.waitKey(0)
 cv2.imwrite("clone.png", clone_img(test_img))
 cv2.imwrite("reversed.png", reverse_img(test_img))
 cv2.imwrite("centerMirrored.png", midmirror_img(test_img))
 cv2.imwrite("rotated90.png", rotate90(test_img))
 cv2.imwrite("avg_blurred.png", avg_blur(test_img))
-
-
+cv2.imwrite("shadow.png", shadowing(test_img, 100))
+cv2.imwrite("shadowV2.png", shadowing_v2(test_img, 30))
+cv2.imwrite("modded.png", mod_img(test_img, 3))
+cv2.imwrite("sqrt.png", sqrt_img(test_img))
 
